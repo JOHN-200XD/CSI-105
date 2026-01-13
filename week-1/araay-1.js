4374983a26add1b7407f8db087e6932be4fcba6b
Kasemsak Ratchanipan <gmjohn200gg@gmail.com>
2026-01-13 23:59:04 +0700

    new

--- week-1/araay-1.js
+++ week-1/araay-1.js
@@ -21,6 +21,7 @@ function addData() {
         img.style.display = "inline-block";
         img.src = "https://i.pinimg.com/736x/48/a7/8d/48a78d886c2a3ffbac8472811aced3fc.jpg";
         img.style.padding = "70px";
+        img.style.borderRadius = "0px";
         img.style.height = "350px";
         img.style.width = "400px";;
     } if (num % 2 !== 0){
@@ -29,6 +30,7 @@ function addData() {
         img.style.display = "inline-block";
         img.src = "https://i.pinimg.com/736x/7b/b4/5f/7bb45f567a122959248cb025c46952ad.jpg";
         img.style.padding = "70px";
+        img.style.borderRadius = "0px";
         img.style.height = "350px";
         img.style.width = "400px";
      
@@ -37,6 +39,7 @@ function addData() {
         img.style.display = "inline-block";
         img.src = "https://i.pinimg.com/736x/fe/c3/58/fec3580c3cf476955bdadf12926c4ffa.jpg";
         img.style.padding = "70px";
+        img.style.borderRadius = "0px";
         img.style.height = "650px";
         img.style.width = "650px";
         out.style.fontSize = "50px";
